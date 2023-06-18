b7094bfa178bc8652dc740cfa30b2aa0e1b80a8b
Alexx Sukhov <alexx.fr-man@yandex.ru>
2023-06-18 20:35:23 +0300

    add tasks for seminar 6

--- immersion_in_python/seminar_1/task_3.py
+++ immersion_in_python/seminar_1/task_3.py
@@ -4,16 +4,21 @@
 Сделайте ограничение на ввод отрицательных чисел и чисел больше 100 тысяч.
 """
 
-usr_number = int(input("Введите число от 1 до 10 000"))
+usr_number = int(input("Введите число от 1 до 10 000: "))
+
+while usr_number > 10000 or usr_number < 1:
+    print("Вы ввели некорретное значение")
+    usr_number = int(input("Введите число от 1 до 10 000: "))
+
 count_divider = 0
 
 start_range = 2
 
-for i in range(start_range, usr_number):
+for i in range(start_range, usr_number // 2):
     if usr_number % i == 0:
         count_divider += 1
 
-if count_divider > 2:
-    print("Число не является простым")
+if count_divider >= 2:
+    print(f"Число {usr_number} не является простым")
 else:
-    print("Число является простым")
+    print(f"Число {usr_number} является простым")
