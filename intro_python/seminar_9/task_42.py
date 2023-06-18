28bf36d91c8975bb528b3556e1c4898083077b84
Alexx Sukhov <129314805+alexxsukhov@users.noreply.github.com>
2023-06-18 19:48:44 +0300

    Delete intro_python directory
    del other info

--- intro_python/seminar_9/task_42.py
+++
@@ -1,7 +0,0 @@
-# Задача 42: Узнать какая максимальная households в зоне минимального значения population.
-
-import pandas as pd
-
-data = pd.read_csv('california_housing_test.csv')
-
-print(data[data['population'] == data['population'].min()]['households'].max())
